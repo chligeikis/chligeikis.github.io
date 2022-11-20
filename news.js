ced8362b43beb1a9aabda33dcb6d708d9c44ee85
chligeikis <ligeikis@umich.edu>
2022-11-19 20:18:36 -0500

    woohoo

--- news.js
+++ news.js
@@ -1,7 +1,45 @@
 news = [
     {
         "date": "May 2022",
-        "body": "I attended the inaugural Cyber-Physical Systems (CPS) Rising Stars Workshop at the University of Virginia",
+        "body": "I attended the inaugural <i>Cyber-Physical Systems (CPS) Rising Stars </i> Workshop at the University of Virginia",
         "url": "https://risingstars.linklab.virginia.edu/2022/index.html"
     }
+,
+    {
+        "date": "April 2021",
+        "body": "I received my MS in Electrical \& Computer Engineering (Control Systems) from the University of Michigan",
+        "url": ""
+    }
+,
+    {
+        "date": "June 2019",
+        "body": "I began a summer internship at Sandia National Laboratories as part of the <i>Nonlinear Mechanics and Dynamics (NOMAD) Research Institute</i>",
+        "url": ""
+    }
+,
+    {
+        "date": "May 2019",
+        "body": "I received my MS in Civil Engineering (Structural Engineering) from the University of Connecticut",
+        "url": ""
+    }
+,
+    {
+        "date": "February 2019",
+        "body": "I attended the PREEMPTIVE (Pacific Rim Earthquake Engineering Mitigation Protective Technologies International Virtual Environment) Advanced Studies Institute at the University of Costa Rica",
+        "url": ""
+    }
+,
+    {
+        "date": "July 2018",
+        "body": "I attended the <i> Substructuring in Engineering Dynamics: Emerging Numerical and Experimental Techniques </i> short course at the International Center for Mechanical Sciences in Udine, Italy",
+        "url": ""
+    }
+,
+    {
+        "date": "May 2017",
+        "body": "I received my BSE in Civil Engineering (<i>summa cum laude</i>) from the University of Connecticut",
+        "url": ""
+    }
+
+
 ]
